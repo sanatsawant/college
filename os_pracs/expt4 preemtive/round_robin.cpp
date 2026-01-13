c06643240ca06648a90e343fba996136bcd20ae2
sanatsawant <sanatsawantgvms@gmail.com>
2026-01-13 19:33:40 +0530

    Update round_robin.cpp

--- os_pracs/expt4 preemtive/round_robin.cpp
+++ os_pracs/expt4 preemtive/round_robin.cpp
@@ -1,100 +1,107 @@
-// #include<iostream>
-// #include<vector>
-// #include<queue>
-// #include<algorithm>
-// #include<utility>
-// #include<iomanip>
-// #define sp setw(10)
-// using namespace std;
-// int n;
-// class process{
-//     public:
-//     int at;
-//     int bt;
-//     int orig_bt;
-//     int ct;
-//     int id;
-//     int tat;
-//     int wt;
-//     bool done;
-//     bool in_queue;
-//     process(){
-//         at=bt=ct=id=tat=wt=0;
-//         done=false;
-//         in_queue=false;
-//     }
-// };
+#include<iostream>
+#include<vector>
+#include<iomanip>
+#include<queue>
+#define MAX 20
+#define sp setw(5)
+using namespace std;
 
-// vector<process> v(10000);
-// void sortby_at(vector<int>& t){
-//     int m=t.size();
-//     for(int i=0;i<m;i++){
-//         for(int j=0;j<m-1;j++){
-//             if(v[t[j]].at>v[t[j+1]].at) swap(t[j],t[j+1]);
-//         }
-//     }
-// }
-// int main(){
-//     cin>>n;
-//     int tq;cin>>tq;
-    
-//     for(int i=0;i<n;i++){
-//         cin>>v[i].at>>v[i].bt;
-//         v[i].orig_bt=v[i].bt;
-//         v[i].id=i;
-//     }
+vector<int> gantt;
+class process{
+    public:
+    int id;
+    int at;
+    int bt;
+    int orig_bt;
+    int ct;
+    int tat;
+    int wt;
+    bool done;
+    bool inqueue;
+    process(){
+        inqueue=done=false;
+    }
+};
+process v[MAX];
+int main(){
+    int n;cin>>n;
+    int tq;cin>>tq;
+    for(int i=0;i<n;i++){
+        cin>>v[i].at>>v[i].bt;
+        v[i].orig_bt=v[i].bt;
+        v[i].id=i+1;
+    }
+    //sort based on at
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-1;j++){
+            if(v[j].at>v[j+1].at) swap(v[j],v[j+1]);
+        }
+    }
+    queue<process> q;
+    int completed=0;
+    int curr=0;
+    while(completed<n){
+        int f=0;
+        int a=0;
+        int id;
+        if(q.size()){
+            a=1;
+            id=q.front().id;
+            int idx=-1;
+            for(int i=0;i<n;i++){
+                if(v[i].id==id){
+                    idx=i;
+                    break;
+                }
+            }
+            q.pop();
+            
+            curr+=min(v[idx].bt,tq);
+            v[idx].bt-=min(v[idx].bt,tq);
+            gantt.push_back(v[idx].id);
+            if(v[idx].bt==0){
+                v[idx].ct=curr;
+                v[idx].done=true;
+                v[idx].tat=v[idx].ct-v[idx].at;
+                v[idx].wt=v[idx].tat-v[idx].orig_bt;
+                completed++;
+                f=1;
 
-//     int curr=0;
-//     int completed=0;
-//     queue<int> q;
+            }
 
-//     while(completed < n){
+        }
+        for(int i=0;i<n;i++){
+            if(v[i].at<=curr && !v[i].done && !v[i].inqueue){
+                q.push(v[i]);
+                v[i].inqueue=true;
 
-//         // add newly arrived processes
-//         for(int i = 0; i < n; i++){
-//             if(!v[i].done && !v[i].in_queue && v[i].at <= curr){
-//                 q.push(i);
-//                 v[i].in_queue = true;
-//             }
-//         }
+            }
+        }
+        if(a && f==0){
+            for(int i=0;i<n;i++){
+                if(v[i].id==id){
+                    q.push(v[i]);
+                    break;
+                }
+            }
 
-//         if(q.empty()){
-//             curr++;
-//             continue;
-//         }
+        }
+        if(!q.size()){
+            curr++;
+        }
+    }
+    cout<<"id"<<sp<<"at"<<sp<<"bt"<<sp<<"ct"<<sp<<"tat"<<sp<<"wt"<<endl;
+    for(int id=1;id<=n;id++){
+        for(int i=0;i<n;i++){
+            if(id==v[i].id){
+                cout<<v[i].id<<sp<<v[i].at<<sp<<v[i].orig_bt<<sp<<v[i].ct<<sp<<v[i].tat<<sp<<v[i].wt<<endl;
+                break;
+            }
+        }
+    }
 
-//         int p = q.front();
-//         q.pop();
-
-//         int exec = min(v[p].bt, tq);
-//         v[p].bt -= exec;
-//         curr += exec;
-
-//         // add arrivals during execution
-//         for(int i = 0; i < n; i++){
-//             if(!v[i].done && !v[i].in_queue && v[i].at <= curr){
-//                 q.push(i);
-//                 v[i].in_queue = true;
-//             }
-//         }
-
-//         if(v[p].bt == 0){
-//             v[p].ct = curr;
-//             v[p].tat = v[p].ct - v[p].at;
-//             v[p].wt = v[p].tat - v[p].orig_bt;
-//             v[p].done = true;
-//             completed++;
-//         } else {
-//             q.push(p);
-//         }
-// }
-
-
-//     cout<<"id"<<sp<<"at"<<sp<<"bt"<<sp<<"ct"<<sp<<"tat"<<sp<<"wt"<<endl;
-//     for(int i=0;i<n;i++){
-//         cout<<v[i].id<<sp<<v[i].at<<sp<<v[i].orig_bt<<sp<<v[i].ct<<sp<<v[i].tat<<sp<<v[i].wt<<endl;
-//     }
-
-
-
-// }
+    cout<<"gantt chart\n";
+    for(auto x:gantt){
+        cout<<"P"<<x<<' ';
+    }
+}
